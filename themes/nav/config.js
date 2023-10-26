82bef120ba9d6d01fbc97cc9c4a492d00c3a3ed1
听海风 <112362648+hqd1102@users.noreply.github.com>
2023-10-26 14:58:33 -0500

    Update config.js

--- themes/nav/config.js
+++ themes/nav/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
 
   AUTO_SORT: process.env.NEXT_PUBLIC_GITBOOK_AUTO_SORT || true, // 是否自动按分类名 归组排序文章；自动归组可能会打乱您Notion中的文章顺序
 
-  SHOW_TITLE_TEXT: false, // 标题栏显示文本
+  SHOW_TITLE_TEXT: true, // 标题栏显示文本
   USE_CUSTOM_MENU: true, // 使用自定义菜单（可支持子菜单，支持自定义分类图标），若为true则显示所有的category分类
 
   // 菜单
@@ -14,7 +14,7 @@ const CONFIG = {
   MENU_SEARCH: true, // 显示搜索
 
   // Widget
-  WIDGET_REVOLVER_MAPS: process.env.NEXT_PUBLIC_WIDGET_REVOLVER_MAPS || 'false', // 地图插件
+  WIDGET_REVOLVER_MAPS: process.env.NEXT_PUBLIC_WIDGET_REVOLVER_MAPS || 'true', // 地图插件
   WIDGET_TO_TOP: true // 跳回顶部
 }
 export default CONFIG
